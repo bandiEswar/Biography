4a6932e4768e475d4ecc4c2a6b870d94e49079af
bandiEswar <121429247+bandiEswar@users.noreply.github.com>
2023-08-03 22:06:57 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,24 @@
+ReactDOM.render(
+    <div>
+        <h1>Subhas Chandra Bose</h1>
+        <div classname="container">
+        <img classname="Image" src="https://upload.wikimedia.org/wikipedia/commons/4/44/Subhas_Chandra_Bose_NRB.jpg" height="600px" width="auto"></img>
+        </div>
+        <p>Subhas Chandra Bose was a leader who wanted India to be free from British rule.</p>
+        <p>He is also called Netaji, which means “Respected Leader”.</p>
+        <h3>Early life and education</h3>
+        <p>Bose was born on 23 January 1897 in Cuttack, a city in India. His family was rich and powerful. He studied in India and England. He passed a very hard exam to work for the British government in India. But he quit his job and came back to India in 1921. He joined the group of people who were fighting for India’s freedom.</p>
+        <h3>Political career and imprisonment</h3>
+        <p>Bose was a brave and strong leader. He wanted to fight against the British with guns and bombs. He was put in jail many times by the British for his actions. He did not agree with Gandhi and other leaders who wanted to use peaceful ways to get freedom. He made his own group called the Forward Bloc in 1939</p>
+        <h3>Escape and alliance with Germany and Japan</h3>
+        <p>In 1941, Bose ran away from his house where the British were watching him. He went to Germany, where he met Hitler. He asked Hitler to help him free India from the British. He made an army of Indian soldiers who were captured by the Germans. They fought with the Germans in the war.</p>
+        <p>In 1943, Bose left Germany and went to Japan. Japan was also fighting against the British in Asia. Bose became the leader of another army of Indian soldiers who had joined the Japanese. He also made a government of Free India in Singapore. Japan and its friends said that they accepted his government.</p>
+        <h3>Greatest work for India</h3>
+        <p>Bose’s greatest work for India was his leadership of the Indian National Army (INA), which he formed with the help of Japan during World War II. The INA was an army of Indian soldiers and civilians who had joined the Japanese invasion of India from Southeast Asia. Bose wanted to use the INA to liberate India from British rule by force. He also declared a provisional government of Free India in Singapore, which was recognized by Japan and its allies123</p>
+        <p>Bose and the INA fought bravely against the British forces in India, especially in Manipur and Nagaland. They also tried to inspire the Indian people to rise up against the British. They used the slogan “Jai Hind” (Victory to India) and the salutation “Netaji” (Respected Leader) to express their patriotism and loyalty to Bose. They also adopted a tricolour flag with a springing tiger as the emblem of Free India123</p>
+        <p>Bose’s work for India was very important because he showed that Indians could fight against the British with courage and determination. He also challenged the British claim that they were ruling India for its own good. He made the British realize that they could not hold on to India for long. He also inspired many Indians to join the freedom struggle and to demand complete independence from Britain</p>
+        <h3>Death and legacy</h3>
+        <p>Bose and his army fought against the British in India. They won some battles but lost others. The war ended when Japan gave up in August 1945. Bose died in a plane crash on 18 August 1945 in Taipei, a city in Taiwan. He was trying to go to another country. Some people do not believe that he died in the crash. They think he lived longer.</p>
+        <p>Bose is very important in India’s history. Many people love him as a hero and a martyr. Some people do not like him because he worked with Hitler and Japan. His life and work still inspire many people today.</p>
+            </div>,document.getElementById("biography")
+);
